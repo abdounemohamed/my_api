ed3f6545b85e11919e3f22a2c2fb1580cd764849
abdoune133 abdoune133 <abdoune4s@gmail.com>
2023-01-29 21:06:05 +0100

    start tests

---
+++ tests/Feature/ProductsTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProductsTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_post_key()
+    {
+        $this->json('POST', 'api/v1/key/post', ['key' => 'test', 'value' => 'test', 'timestamp' => 1674996297])
+            ->assertStatus(201)
+            ->assertJson([
+                'message' => 'key created with success'
+            ]);
+    }
+}
