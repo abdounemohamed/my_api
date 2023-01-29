8f2e84e8c72c09ff08db7d6cee500d9086802509
abdoune133 abdoune133 <abdoune4s@gmail.com>
2023-01-29 17:34:11 +0100

    fix composer i

--- deploy.sh
+++ deploy.sh
@@ -5,24 +5,24 @@ echo "Deployment started ..."
 
 # Enter maintenance mode or return true
 # if already is in maintenance mode
-(php artisan down) || true
+(/usr/bin/php8.0 artisan down) || true
 
 # Pull the latest version of the app
 git pull origin main
 
 # Install composer dependencies
- /usr/bin/php8.0 composer.phar install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+/usr/bin/php8.0 composer.phar install --no-dev --no-interaction --prefer-dist --optimize-autoloader
 
 # Clear the old cache
-php artisan clear-compiled
+/usr/bin/php8.0 artisan clear-compiled
 
 # Recreate cache
-php artisan optimize
+/usr/bin/php8.0 artisan optimize
 
 # Run database migrations
-php artisan migrate --force
+/usr/bin/php8.0 artisan migrate --force
 
 # Exit maintenance mode
-php artisan up
+/usr/bin/php8.0 artisan up
 
 echo "Deployment finished!"
